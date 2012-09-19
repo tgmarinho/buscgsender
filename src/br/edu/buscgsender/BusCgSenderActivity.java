db78fd4653a405aa1ac2ab7c6f22d8e3122a6086
tgmarinho <tgmarinho@gmail.com>
2012-09-19 12:45:51 -0400

    estrutura do projeto android 2.2

---
+++ src/br/edu/buscgsender/BusCgSenderActivity.java
@@ -0,0 +1,13 @@
+package br.edu.buscgsender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BusCgSenderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
